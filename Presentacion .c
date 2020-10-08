4f9220963c39a50b79a7414035a1a7375545d081
JocsanMoreno <71853949+JocsanMoreno@users.noreply.github.com>
2020-10-07 19:14:41 -0500

    Creacion de presentacion c
    Se creo archivo con codigo fuente

---
+++ Presentacion .c
@@ -0,0 +1,14 @@
+#include<stdio.h> //stdio -> Standard Input/Output
+
+int main(){
+	/*My first
+	 * Program
+	 * in C
+	 */
+	 /*A veces siento que esto es dificil pero creo 
+	 que solo me ahogo dentro de un vaso de agua*/
+	  
+	printf("Hola mundo, me llamo: ");
+	printf("Jocsan Moreno Catalan!");
+	return 0;
+}
